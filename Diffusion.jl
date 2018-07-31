82cba2f08a27617f92699e0cd1e871f76b01b2fb
Francesco Lovascio <f.lovascio@qmul.ac.uk>
2018-07-31 19:35:21 +0100

    dust test works

--- Diffusion.jl
+++ Diffusion.jl
@@ -5,14 +5,14 @@ function makeWave(t::Float64, init::Array{Float64})
 	h=L/length(init)
 	for i=1:length(init)
 		x=h*i
-		wave[i]=real(P0+abs(P1)*exp(im*(Omeg*t+k*x+angle(P1))))
+		wave[i]=real(P0-abs(P1)*exp(im*(Omeg*t+k*x+angle(P1))))
 	end
 	return wave
 end
 
 function LN(data::Array{Float64},theoretical::Array{Float64},N::Float64)
 	L=0.
-	for i=0:length(data)
+	for i=1:length(data)
 		L=L+abs(data[i]^N -theoretical[i]^N)
 	end
 	L=(1./convert(Float64,length(data)))*L
